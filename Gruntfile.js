55a763f8c6e395489ebd188be061d723560c677c
Patrick Gidich <patrick@simnova.com>
2013-02-21 13:31:00 -0500

    Updated Documentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,13 +5,15 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
 
+    pkg: grunt.file.readJSON("package.json"),
+
     // The lint task will run the build configuration and the application
     // JavaScript through JSHint and report any errors.  You can change the
     // options for this task, by reading this:
     // https://github.com/cowboy/grunt/blob/master/docs/task_lint.md
     jslint: {
       files: [
-        "omnitureHelper.js",
+        "lib/omnitureHelper.js",
         "spec/*.js"
       ]
     },
@@ -19,19 +21,34 @@ module.exports = function(grunt) {
     // The headless Jasmine testing is provided by grunt-jasmine-task. Simply
     // point the configuration to your test directory.
     jasmine: {
-      src: ["omnitureHelper.js"],
+      src: ["lib/omnitureHelper.js"],
       options: {
         specs: ["spec/*.spec.js"],
         vendor: ["vendor/*.js"]
       }
     },
 
+    yuidoc: {
+      compile: {
+        name: "<%= pkg.name %>",
+        description: "<%= pkg.description %>",
+        version: "<%= pkg.version %>",
+        url: "<%= pkg.homepage %>",
+        options: {
+          paths: ["lib/"],
+          outdir: "docs/"
+        }
+      }
+    }
+
   });
 
   grunt.loadNpmTasks('grunt-jslint');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-yuidoc');
 
-  grunt.registerTask("test", ["jslint", "jasmine"]);
+  grunt.registerTask("docs", ["yuidoc"]);
+  grunt.registerTask("test", ["jslint", "jasmine","yuidoc"]);
   grunt.registerTask("release", "jslint");
 };
